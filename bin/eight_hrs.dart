ebc78c5952dbb411697ec5ced86d9cd6dcebcdb2
sumit001007 <sumitapp22@gmail.com>
2021-10-27 00:49:03 +0530

    first commit

---
+++ bin/eight_hrs.dart
@@ -0,0 +1,5 @@
+import 'package:eight_hrs/eight_hrs.dart' as eight_hrs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${eight_hrs.calculate()}!');
+}
---
+++ test/eight_hrs_test.dart
@@ -0,0 +1,8 @@
+import 'package:eight_hrs/eight_hrs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
